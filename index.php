3f431f10b43160c6de2350853de2b07a1c269404
devlakh <devlakh4438@gmail.com>
2022-06-29 22:23:31 +0800

    Integrating MVC

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 require_once("models/includes/helpers.php");
 
 if(isset($_GET["page"])) $page = $_GET["page"];
-else $page = "default";
+else $page = "landing";
 
 switch($page)
 {
-    case "default":
-        render("input_form");
+    case "landing":
+        render("templates/head");
+        render("home");
+        render("templates/foot");
+        break;
+    
+    case "entry":
+        render("templates/head");
+        render("entry");
+        render("templates/foot");
+        break;
+
+    case "entries":
+        render("templates/head");
+        render("entries");
+        render("templates/foot");
         break;
 
     case "createPDF":
