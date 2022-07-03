4056154478267f3849cca0824619b892d7f26d90
devlakh <devlakh4438@gmail.com>
2022-07-03 21:03:34 +0800

    Option Text Recovery(change onclick)

--- assets/js/getProducts.js
+++ assets/js/getProducts.js
@@ -1,21 +1,33 @@
-function getProducts(_formData){
-
+function getProducts(_formData)
+{
 	fetch('models/getProducts.php', {
 		method: 'POST'
 	})
 	.then(response => response.json())
 	.then(results => {
+		
+		// Placeholder Option
+		let elem = document.createElement("option");
+		elem.innerText = "---Select Option---";
 
+		elem.onclick = ()=>{
+			elem.innerText = "---Select Option---";
+			_formData.append('coolKey', -1);
+		}
+		document.querySelector("#productList").append(elem);
+
+		// Options From Database
 		results.forEach(result => {
 			let elem = document.createElement("option");
 			elem.innerText = result.name;
+
 			elem.onclick = ()=>{
-				console.log(result.id);
+				elem.innerText = result.name;
 				_formData.append('coolKey', result.id);
 			}
+
 			document.querySelector("#productList").append(elem);
 		});
 
 	});
-
 }
